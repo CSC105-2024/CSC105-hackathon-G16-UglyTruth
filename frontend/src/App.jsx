91a5adba86ffa9220e8a52eb449fa1fad2741a0a
MiDtsuki <myothetpaing2004@gmail.com>
2025-06-01 00:24:27 +0700

    ui: postcard and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login';
-// import PrivatePosts from './pages/PrivatePosts';
-// import PublicPosts from './pages/PublicPosts';
+import Login from './pages/LogIn';
+import PrivatePosts from './pages/PrivatePosts';
+import PublicPosts from './pages/PublicPosts';
 import SignUp from './pages/SignUp';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
@@ -12,10 +13,9 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        {/*<Route path="/posts">
-          <Route path="public" element={<PublicPosts />} />
-          <Route path="private" element={<PrivatePosts />} />
-        </Route> */}
+        <Route path="/public-posts" element={<PublicPosts />} />
+        <Route path="/private-posts" element={<PrivatePosts />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import SideBar from '../components/SideBar';
 
-const PrivatePosts = () => {
+const CreatePost = () => {
   return (
     <div>
       <SideBar />
       <div className="flex-1 p-8 text-color-linen">
         {/* Main content area */}
-        <h1 className="text-2xl font-pica mb-6">Private Posts</h1>
-        {/* Add your private posts content here */}
+        <h1 className="text-2xl font-pica mb-6">Create New Post</h1>
+        {/* Add your post creation form/components here */}
       </div>
     </div>
   );
 };
 
-export default PrivatePosts;
+export default CreatePost;
