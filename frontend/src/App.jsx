e6631944be4b1f97a112cb09fb2271ea8cf1b3d0
MiDtsuki <myothetpaing2004@gmail.com>
2025-05-31 17:18:14 +0700

    ui: side bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import UglyTruthLogo from '/UglyTruthLogo.svg'
-import SignUp from './pages/SignUp'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import Login from './pages/Login';
+// import PrivatePosts from './pages/PrivatePosts';
+// import PublicPosts from './pages/PublicPosts';
+// import SignUp from './pages/SignUp';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <SignUp />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/posts">
+          <Route path="public" element={<PublicPosts />} />
+          <Route path="private" element={<PrivatePosts />} />
+        </Route> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from '../components/SideBar';
+
+const Home = () => {
+
+
+
+  return (
+    <div className='flex bg-color-midnight w-screen overflow-hidden'>
+      <SideBar />
+    </div>
+  )
+}
+
+export default Home;
