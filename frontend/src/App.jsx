196dfbf933a053fc3f3344a8fb64e2e716560bc4
May Thu <khiangmaythu528@gmail.com>
2025-05-31 18:18:08 +0700

    sign up page, not responsive yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import UglyTruthLogo from '/UglyTruthLogo.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
-
+//import Login from './pages/LogIn'
+//import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <SignUp />
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+      </Router>
     </>
   )
 }
