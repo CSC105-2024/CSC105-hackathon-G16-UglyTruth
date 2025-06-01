6b759d73d5e13d40e50228956d5491152e634503
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-06-01 07:44:05 +0700

    feat: public private posts, now able to sort

--- backend/prisma/migrations/20250531234557_woof/migration.sql
+++ backend/prisma/migrations/20250531234557_woof/migration.sql
@@ -13,8 +13,11 @@ CREATE TABLE `User` (
 CREATE TABLE `Post` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `upvotes` INTEGER NOT NULL DEFAULT 0,
+    `description` TEXT NOT NULL,
+    `category` VARCHAR(191) NOT NULL DEFAULT 'other',
+    `warning` BOOLEAN NOT NULL DEFAULT false,
+    `relatableCount` INTEGER NOT NULL DEFAULT 0,
+    `isPublic` BOOLEAN NOT NULL DEFAULT false,
     `authorId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
@@ -23,15 +26,13 @@ CREATE TABLE `Post` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Vote` (
+CREATE TABLE `Relatable` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `type` ENUM('UP') NOT NULL,
     `userId` INTEGER NOT NULL,
     `postId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Vote_userId_postId_key`(`userId`, `postId`),
+    UNIQUE INDEX `Relatable_userId_postId_key`(`userId`, `postId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -39,7 +40,7 @@ CREATE TABLE `Vote` (
 ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Vote` ADD CONSTRAINT `Vote_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Relatable` ADD CONSTRAINT `Relatable_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Vote` ADD CONSTRAINT `Vote_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Relatable` ADD CONSTRAINT `Relatable_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
